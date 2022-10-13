9a6ba3fedd89b0e0a0c3ac7df69a455275305120
ShcMax <102209342+ShcMax@users.noreply.github.com>
2022-10-13 15:11:00 +0300

    h.m.15/10

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -14,16 +14,15 @@ public class Elevator
     public void FixedUpdate()
     {
         var speed = _elevatorView.SliderJoint.motor;
-        speed.motorSpeed = _elevatorView.SpeedMotor;
-        Debug.Log(speed.motorSpeed);
+        speed.motorSpeed = _elevatorView.SpeedMotor;        
 
         if (_elevatorView.transform.position.y <= _elevatorView.PositionY1)
         {
-            
+            speed.motorSpeed = -(_elevatorView.SpeedMotor);            
         }
         if (_elevatorView.transform.position.y >= _elevatorView.PositionY2)
         {
-                        
+            speed.motorSpeed = _elevatorView.SpeedMotor;
         }        
     }
 }
